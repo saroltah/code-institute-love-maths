3286f4900b4e66ea922007486a7b274ed63ac0d6
Sarolta <sarolta.hegyi@gmail.com>
2023-10-30 09:16:33 +0100

    corrected missing line

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,24 @@ document.addEventListener("DOMContentLoaded", function(){
       {
         if (this .getAttribute("data-type")==="submit"){
           alert("You clicked Submit");
-        }else{alert(`you clicked ${gameType}`)
+        }else{
+           let gameType = this.getAttribute("data-type");
+          alert(`you clicked ${gameType}`)
 
         }
       })
   }
 })
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+
 // this refers to the button that was clicked, it gets attribute, if it is submit, it has the function. 
 function runGame(){
-//random number between 1 and 25, math.floor egész számmá kerekíti, +1 hogy ne nulláról induljon, hanem 1ről
+  //random number between 1 and 25, math.floor egész számmá kerekíti, +1 hogy ne nulláról induljon, hanem 1ről
+// Creates two random numbers between 1 and 25
 let num1=Math.floor(Math.random()*25)+1;
 let num2=Math.floor(Math.random()*25)+1;
 }
@@ -36,7 +45,7 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
 
 }
 function displaySubtractQuestion(){
