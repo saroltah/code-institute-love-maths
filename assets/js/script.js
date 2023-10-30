1077d4d5953e4a0dde93fad60a2c5c8ee0b9e257
Sarolta <sarolta.hegyi@gmail.com>
2023-10-30 20:45:30 +0100

    add calculate points functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(){
       button.addEventListener("click", function()
       {
         if (this .getAttribute("data-type")==="submit"){
-          alert("You clicked Submit");
+          checkAnswer{};
         }else{
            let gameType = this.getAttribute("data-type");
           alert(`you clicked ${gameType}`)
@@ -40,8 +40,23 @@ let num2=Math.floor(Math.random()*25)+1;
     }
 
 }
+/**
+ * Checks the answer agaist the first element in
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer(){
+  let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+             incrementScore();
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+         incrementWrongAnswer();
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 function calculateCorrectAnswer(){
  let operand1 = parseInt(document.getElementById('operand1').innerText);
@@ -58,11 +73,26 @@ function calculateCorrectAnswer(){
 function incrementCorrectAnswer(){
 
 }
+
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+
 function incrementScore(){
+ let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
-function incrementWrongAnswer(){
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer(){
+function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
 }
 function displayAdditionQuestion(operand1, operand2){
 document.getElementById('operand1').textContent = operand1;
