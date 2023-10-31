d8acfcd43bb905c7a5a3c3059a249c605c3dc2a6
Sarolta <sarolta.hegyi@gmail.com>
2023-10-31 17:25:55 +0100

    add multiply question

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,8 @@ let num2=Math.floor(Math.random()*25)+1;
 
  if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+        } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -65,6 +67,8 @@ function calculateCorrectAnswer(){
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+         } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -103,5 +107,8 @@ function displaySubtractQuestion(){
 
 }
 function displayMultiplyQuestion(){
+document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
